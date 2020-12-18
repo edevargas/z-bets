991ba7967d54e1f68e509c2ad1be6399e55002ef
Javier Albadan <javier.albadan@zemoga.com>
2020-12-18 17:58:45 -0500

    feat: Progress getting data from Firestore

---
+++ pages/history.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>Histórico de partidos ya jugados</div>
+</template>
+
+<script>
+export default {
+    name: 'History',
+}
+</script>
---
+++ pages/results.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>Resultados de apuestas en tiempo real</div>
+</template>
+
+<script>
+export default {
+    name: 'Results',
+}
+</script>
