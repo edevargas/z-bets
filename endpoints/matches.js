8bf64d551ac2dbac1245170a746e59c3f348430e
Javier <javier.albadan@zemoga.com>
2021-01-31 17:22:21 -0500

    feat: Create helper function to process dates in readable text

--- endpoints/matches.js
+++ endpoints/matches.js
@@ -1,3 +1,5 @@
+import { getObjectWithDate } from '~/plugins/helpers'
+
 export function getNextMatch() {
     const { firestore } = $nuxt.$fire
     const matches = firestore.collection('matches')
@@ -5,7 +7,8 @@ export function getNextMatch() {
     const query = matches.where("date", ">", today).limit(1)
     query.onSnapshot((querySnapshot) => {
         querySnapshot.forEach((doc) => {
-            $nuxt.$emit('next-match', doc.data())
+            const nextMatch = getObjectWithDate(doc.data())
+            $nuxt.$emit('next-match', nextMatch)
         });
     });
 }
