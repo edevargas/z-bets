e7dc36d321b52699104d26027c848ff64bf3c71e
Juan Castañeda <jcastan@US-C02ZD0AKLVDT>
2021-02-25 15:19:28 -0500

    feat: sendSlackNotifications cloud function added

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -5,10 +5,25 @@ module.exports = {
     node: true,
   },
   extends: [
-    "eslint:recommended",
-    "google",
+    'eslint:recommended',
   ],
   rules: {
-    quotes: ["error", "double"],
+    'arrow-parens': ['error', 'always'],
+    'comma-dangle': 'off',
+    indent: [1, 2, {
+      ignoredNodes: ['TemplateLiteral'],
+    }],
+    'max-len': [1, { 'code': 120 }],
+    'no-empty-pattern': 'off',
+    'no-tabs': ['error', { allowIndentationTabs: true }],
+    'no-undef': 'off',
+    'no-unused-expressions': 'off',
+    'no-unused-vars': ['error', { 'args': 'none' }],
+    'object-curly-spacing': [1, 'always'],
+    'prefer-template': 1,
+    'space-before-function-paren': ['error', 'never'],
+    quotes: [1, 'single'],
+    semi: [1, 'always'],
+    'template-curly-spacing': 'off',
   },
 };
--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
-// const functions = require("firebase-functions");
+const functions = require('firebase-functions');
+const axios = require('axios');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,3 +8,19 @@
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+exports.sendSlackNotifications = functions.firestore
+  .document('bets/{betId}').onCreate((change, context) => {
+    console.log('test', context);
+    axios
+      .post('https://hooks.slack.com/services/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', {
+        text: 'Se registró una nueva apuesta',
+      })
+      .then((response) => {
+        console.log(`statusCode: ${response.statusCode}`);
+        console.log(response);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  });
