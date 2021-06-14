f656956eea91504e2e21426aed30a97d972d60ae
javier.albadan <javier.albadan@zemoga.com>
2021-06-13 19:07:05 -0500

    Fixing flag-mapper in cloud functions

--- functions/components/utils/flag-mapper.js
+++ functions/components/utils/flag-mapper.js
@@ -1,13 +1,14 @@
 const FLAG_MAPPER = {
-  col: ':flag-co:',
-  per: ':flag-pe:',
   arg: ':flag-ar:',
-  par: ':flag-py:',
   bol: ':flag-bo:',
-  ven: ':flag-ve:',
-  chi: ':flag-cl:',
-  uru: ':flag-uy:',
   bra: ':flag-br:',
+  chi: ':flag-cl:',
+  col: ':flag-co:',
+  ecu: ':flag-ec:',
+  par: ':flag-py:',
+  per: ':flag-pe:',
+  uru: ':flag-uy:',
+  ven: ':flag-ve:',
 };
 
 const getSlackFlag = (teamId = '') => FLAG_MAPPER[teamId] || '';
